07963ba6f7667c450e43fa89ba7e6620a3610868
00752-tech <passiveincomestreaming@gmail.com>
2025-06-30 09:29:52 -0700

    Update route.ts

--- app/api/sale/route.ts
+++ app/api/sale/route.ts
@@ -1,6 +1,6 @@
 import { redirect } from 'next/navigation'
 
 export async function GET() {
-  redirect('https://hop.clickbank.net/?affiliate=smartdiet3&vendor=provive&tid=vsl')
+  redirect('https://a2e71-b0p89p8o8i2j3c643zc2.hop.clickbank.net')
 }
 
